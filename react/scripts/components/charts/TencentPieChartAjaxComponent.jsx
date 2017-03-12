a54cc404d2232263680bfe6abf939e606bce7b82
ruixie <ruixie@carfax.com>
2017-03-11 23:38:11 -0600

    able to get graph to show, but not stable..

--- react/scripts/components/charts/TencentPieChartAjaxComponent.jsx
+++ react/scripts/components/charts/TencentPieChartAjaxComponent.jsx
@@ -4,70 +4,67 @@ import $ from 'jquery';
 
 const TencentChartAPIAjaxComponent = React.createClass({
     propTypes: {},
-    // componentDidMount: function () {
-    //     var languages = [];
-    //     var percentages = [];
-    //     $.ajax({
-    //         url: 'http://127.0.0.1:8080/key/getReport',
-    //         dataType: 'json',
-    //         success: function (result) {
-    //             if (result) {
-    //                 for (var i = 0; i < result.languages.length; i++) {
-    //                     languages.push(result["languages"][i]["name"]);
-    //                     percentages.push(result["languages"][i]["percentage"]);
-    //                 }
-    //                 this.setOption({
-    //                         xAxis: {
-    //                             data: languages
-    //                         },
-    //                         series: [{
-    //                             name: '语言',
-    //                             data: percentages
-    //                         }]
-    //                     }
-    //                 );
-    //             }
-    //         },
-    //         error: function (errorMsg) {
-    //             alert("图表请求数据失败!");
-    //         }
-    //     })
-    // },
+    getInitialState: function () {
+        return {option: this.getOption()};
+    },
+    fetchNewData: function () {
+        var languages = [];
+        var percentages = [];
+        let option = this.state.option;
+        $.ajax({
+            url: 'http://127.0.0.1:8080/key/getReport',
+            dataType: 'json',
+            success: function (result) {
+                if (result) {
+                    for (var i = 0; i < result.languages.length; i++) {
+                        languages.push(result["languages"][i]["name"]);
+                        percentages.push(result["languages"][i]["percentage"]);
+                    }
+                    option.xAxis.data = languages;
+                    option.series.data = percentages;
+                    console.log("渲染完毕..")
+                }
+            },
+            error: function (errorMsg) {
+                alert("图表请求数据失败!");
+            }
+        })
+        this.setState({option: option});
+    },
+    componentDidMount: function () {
+        this.fetchNewData()
+    },
+    componentWillUnmount: function() {
+        this.fetchNewData()
+    },
     getOption: function () {
         const option = {
-                title: {
-                    text: 'Ajax Example',
-                    subtext: '技术分布'
-                },
-                tooltip: {},
-                legend: {
-                    data: ['类别']
-                },
-                xAxis: {
+            title: {
+                text: 'Ajax Example',
+                subtext: '技术分布'
+            },
+            tooltip: {},
+            legend: {
+                data: ['类别']
+            },
+            xAxis: {
+                data: []
+            },
+            yAxis: {},
+            series: [
+                {
+                    name: '类别',
+                    type: 'bar',
                     data: []
-                },
-                yAxis: {},
-                series: [
-                    {
-                        name: '类别',
-                        type: 'bar',
-                        data: [
-                            {value: 60, name: 'Java'},
-                            {value: 40, name: 'C++'},
-                            {value: 20, name: 'Python'},
-                            {value: 80, name: 'Groovy'},
-                            {value: 100, name: 'Swift'}
-                        ]
-                    }
-                ]
-            }
-            ;
+                }
+            ]
+        };
         return option;
     },
     render: function () {
         return (
             <div className='examples'>
-                <ReactEcharts option={this.getOption()}/>
+                <ReactEcharts ref='echarts_react' option={this.state.option}/>
             </div>
         );
     }
--- react/scripts/routes.js
+++ react/scripts/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, Redirect } from 'react-router';
 
 import TencentChartAPIAjaxComponent from './components/charts/TencentPieChartAjaxComponent';
+// import DynamicChartComponent from './components/charts/DynamicChartComponent';
 var router = (
     <Router>
         <Route path="/" component={TencentChartAPIAjaxComponent}>
